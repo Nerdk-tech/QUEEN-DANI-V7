ad08a982573de5150f4835ec7567cf71ee4e529c
Nerdk <univversehackers@gmail.com>
2025-11-08 18:29:04 +0100

    Add bot configuration settings in settings.js

---
+++ settings.js
@@ -0,0 +1,28 @@
+//  Bot Configuration File 
+// Edit these values to personalize your bot
+
+const settings = {
+  // 🏷️ Sticker Settings
+  packname: 'Dami4k',
+  author: 'Dami',
+
+  // 🤖 Bot Identity
+  botName: '𝒬𝓊𝑒𝑒𝓃 𝒟𝒶𝓃𝒾 𝒱7',
+  botOwner: 'Dev dami',   // Change to your name if needed
+  ownerNumber: '2349120185747',        // Your number without + symbol
+
+  // 🎬 APIs
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq', // For GIF commands
+
+  // ⚙️ Bot Mode
+  commandMode: 'public', // Options: 'public' (everyone) | 'private' (owner only)
+
+  // 📝 Meta Information
+  description: '💬 A fun, powerful & stylish bot.',
+  version: '7.0.0',
+updateZipUrl: "https://github.com/Nerdk-tech/QUEEN-DANI-V7/archive/refs/heads/main.zip",
+  
+};
+
+// Export so other files can use it
+module.exports = settings;
